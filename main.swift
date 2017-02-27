25d14c30a98ce2b39eed48e56f190f542c27f62f
ciec20151104678 <609182350@qq.com>
2017-02-27 11:35:29 +0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  2017.2.27
+//
+//  Created by 20151104678 on 17/2/27.
+//  Copyright © 2017年 20151104678. All rights reserved.
+//
+
+import Foundation
+var mun = 0
+var i=0
+for j in (1...100){
+    i = i+1
+    mun=mun+i
+    print(j)
+}
+ print(mun)
+
+    
+
+
